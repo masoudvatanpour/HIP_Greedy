6e54c028142ac0ba7aeed49128258620bb530cda
masoudvatanpour <masoud.vatanpour@gmail.com>
2014-10-15 23:25:01 -0600

    input files changed

--- src/HIP/global.java
+++ src/HIP/global.java
@@ -3,6 +3,6 @@ package HIP;
 public class global {
 	public static int hight = 294;
     public static int width = 253;
-    public static String filename =  "myfiles/test.txt";
-    public static final String pictureName = "pics/test.png";
+    public static String filename =  "myfiles/ILS.txt";
+    public static final String pictureName = "pics/ILS.png";
 }
